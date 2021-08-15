d7ace00bc9e9de2ea65eff0ebae0114259c62cd3
negan_tan <negan_tan@qq.com>
2021-08-15 17:22:33 +0800

    封装yaml数据驱动2
    Signed-off-by: negan_tan <negan_tan@qq.com>

--- test_requests/api/wework.py
+++ test_requests/api/wework.py
@@ -8,6 +8,8 @@ class WeWork(BaseApi):
     def __init__(self):
         self.token = Util().get_token()
         self.params["token"] = self.token
+        with open("../api/wework.yaml", encoding="utf-8") as f:
+            self.data = yaml.load(f)
 
     def test_create(self, userid, mobile, name="奥特曼", department=None):
         # access_token = self.get_token()
@@ -33,19 +35,18 @@ class WeWork(BaseApi):
         self.params["mobile"] = mobile
         self.params["name"] = name
         self.params["department"] = department
-        with open("../api/wework.yaml", encoding="utf-8") as f:
-            data = yaml.load(f)
-        return self.send(data["create"])
+        # 将yaml文件读取放到了init方法中，用self.data调用
+        return self.send(self.data["create"])
         # return r.json()
 
     def test_get(self, userid):
         # r = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={self.token}&userid={userid}")
-        data = {
-            "method": "get",
-            "url": f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={self.token}&userid={userid}"
-        }
-        return self.send(data)
-        # return r.json()
+        # data = {
+        #     "method": "get",
+        #     "url": f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={self.token}&userid={userid}"
+        # }
+        self.params["userid"] = userid
+        return self.send(self.data["get"])
 
     def test_update(self, userid, name="奥特曼996"):
         # request_body = {
@@ -54,21 +55,24 @@ class WeWork(BaseApi):
         # }
         # r = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={self.token}",
         #                   json=request_body)
-        data = {
-            "method": "post",
-            "url": f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={self.token}",
-            "json": {
-                "userid": userid,
-                "name": name,
-            }
-        }
-        return self.send(data)
+        # data = {
+        #     "method": "post",
+        #     "url": f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={self.token}",
+        #     "json": {
+        #         "userid": userid,
+        #         "name": name,
+        #     }
+        # }
+        self.params["name"] = name
+        self.params["userid"] = userid
+        return self.send(self.data["update"])
 
     def test_delete(self, userid):
         # r = requests.get(
         #     f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={self.token}&userid={userid}")
-        data = {
-            "method": "get",
-            "url": f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={self.token}&userid={userid}"
-        }
-        return self.send(data)
+        # data = {
+        #     "method": "get",
+        #     "url": f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={self.token}&userid={userid}"
+        # }
+        self.params["userid"] = userid
+        return self.send(self.data["delete"])
